77e4d08b5f2d71c1d8a26ad09c1488cc75dfb7f2
Your Name <mohammad-zamani-message@yahoo.com>
2024-05-11 22:15:48 +0330

    get input from user

--- main.sh
+++ main.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+
+while getopts "n:s:h" options; do
+    case ${options} in
+        n)
+           new_member="$OPTARG"
+           ;;
+        s)
+           wanted_member="$OPTARG"
+           ;;
+        h)
+           echo "if you want add a new member use swich 'n' and if you want search somebody's information, use search swich 's'."
+           ;;
+    esac
+done
+
+
+echo $new_member
+echo $wanted_member
